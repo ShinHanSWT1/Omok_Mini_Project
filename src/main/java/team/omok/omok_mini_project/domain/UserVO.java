f3faaf1f250e68fa8ec7f81f0e7c3093302bdc75
wlaos <dkdlfjsl0719@gmail.com>
2025-12-18 08:51:41 +0900

    lobbyDAO

--- src/main/java/team/omok/omok_mini_project/domain/UserVO.java
+++ src/main/java/team/omok/omok_mini_project/domain/UserVO.java
@@ -2,9 +2,14 @@ package team.omok.omok_mini_project.domain;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class UserVO {
-    private String id;
-    private String password;
+    private int userId;
+    private String loginId;
+    private String user_pwd;
+    private Date created_at;
     private String nickname;
+    private String profile_img;
 }
